7ebe0e82513a507c2d1c08d5ea962b6f70d84382
Ayooluwa Jesuniyi <jesuniyig@gmail.com>
2023-06-28 13:12:56 +0100

    water

--- hungry.py
+++ hungry.py
@@ -3,4 +3,6 @@ if hungry=="yes":
     print("eat ewa agonyi")
     print("eat agbado")
 else:
-    print("do your daily task")
+    thirsty=input("are you thirsty?")
+    if thirsty=="yes":
+        print("drink water")
